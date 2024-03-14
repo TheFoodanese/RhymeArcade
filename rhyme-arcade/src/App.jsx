2a90c53b3d193adb75ffc6a804e954931ed0d9b5
Samgreenfairyx <150940744+Samgreenfairyx@users.noreply.github.com>
2024-03-14 16:06:37 +0000

    updated

--- rhyme-arcade/src/App.jsx
+++ rhyme-arcade/src/App.jsx
@@ -1,20 +1,7 @@
-import { useState } from 'react'
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import { Auth0Provider } from '@auth0/auth0-react';
-import './App.css'
-import React, { useState } from 'react';
 import SpotifyIntegration from './SpotifyIntegration';
-
-ReactDOM.render(
-  <Auth0Provider
-    domain="dev-6ziljfb0z6s42dae.us.auth0.com"
-    clientId="FKcS6yZI6zIfbHR4FRhUAM8R0hSkDFre"
-    redirectUri={window.location.origin}
-  >
-    <App />
-  </Auth0Provider>,
-  document.getElementById('root')
-);
 
 const App = () => {
   const [selectedGame, setSelectedGame] = useState(null);
@@ -27,11 +14,18 @@ const App = () => {
   return (
     <div>
       <h1>Welcome to Rhyme Arcade!</h1>
-      {SpotifyIntegration}
       <SpotifyIntegration selectedGame={selectedGame} />
     </div>
   );
 };
 
-
-export default App
+ReactDOM.render(
+  <Auth0Provider
+    domain="dev-6ziljfb0z6s42dae.us.auth0.com"
+    clientId="FKcS6yZI6zIfbHR4FRhUAM8R0hSkDFre"
+    redirectUri={window.location.origin}
+  >
+    <App />
+  </Auth0Provider>,
+  document.getElementById('root')
+);

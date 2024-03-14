bdd2de3ae315ea328d346101b56834b88ed42efd
Samgreenfairyx <150940744+Samgreenfairyx@users.noreply.github.com>
2024-03-14 15:59:39 +0000

    added some spotify code

--- rhyme-arcade/src/App.jsx
+++ rhyme-arcade/src/App.jsx
@@ -1,22 +1,37 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import ReactDOM from 'react-dom';
+import { Auth0Provider } from '@auth0/auth0-react';
 import './App.css'
+import React, { useState } from 'react';
+import SpotifyIntegration from './SpotifyIntegration';
 
-function App() {
-  const [count, setCount] = useState(0)
+ReactDOM.render(
+  <Auth0Provider
+    domain="dev-6ziljfb0z6s42dae.us.auth0.com"
+    clientId="FKcS6yZI6zIfbHR4FRhUAM8R0hSkDFre"
+    redirectUri={window.location.origin}
+  >
+    <App />
+  </Auth0Provider>,
+  document.getElementById('root')
+);
+
+const App = () => {
+  const [selectedGame, setSelectedGame] = useState(null);
+
+  // Function to handle game selection
+  const handleGameSelect = (game) => {
+    setSelectedGame(game);
+  };
 
   return (
-    <>
-      <div className="outer-rec">
-        <div className='inner-rec'>
-          <h1>Please Select your Console</h1>
-        </div>
-        
-      </div>
-      
-    </>
-  )
-}
+    <div>
+      <h1>Welcome to Rhyme Arcade!</h1>
+      {SpotifyIntegration}
+      <SpotifyIntegration selectedGame={selectedGame} />
+    </div>
+  );
+};
+
 
 export default App

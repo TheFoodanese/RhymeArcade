53b73112aacc84ff357be36e53a1b552341be34c
ZoeSobowale <38224967+ZoeSobowale@users.noreply.github.com>
2024-03-14 19:18:09 +0000

    visual showing

--- rhyme-arcade/src/App.jsx
+++ rhyme-arcade/src/App.jsx
@@ -4,17 +4,6 @@ import { Auth0Provider } from '@auth0/auth0-react';
 import './App.css'
 import SpotifyIntegration from './SpotifyIntegration';
 
-ReactDOM.render(
-  <Auth0Provider
-    domain="dev-6ziljfb0z6s42dae.us.auth0.com"
-    clientId="FKcS6yZI6zIfbHR4FRhUAM8R0hSkDFre"
-    redirectUri={window.location.origin}
-  >
-    <App />
-  </Auth0Provider>,
-  document.getElementById('root')
-);
-
 const App = () => {
   const [selectedGame, setSelectedGame] = useState(null);
 
@@ -37,5 +26,15 @@ const App = () => {
   );
 };
 
+ReactDOM.render(
+  <Auth0Provider
+    domain="dev-6ziljfb0z6s42dae.us.auth0.com"
+    clientId="FKcS6yZI6zIfbHR4FRhUAM8R0hSkDFre"
+    redirectUri={window.location.origin}
+  >
+    <App />
+  </Auth0Provider>,
+  document.getElementById('root')
+);
 
 export default App

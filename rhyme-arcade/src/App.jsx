cf1d6f40e0e841eecd55b95d9de53abd8c30204d
Lowell Hallassou <lo@Lowells-MacBook-Air.local>
2024-03-19 18:59:05 +0000

    Adding game list

--- rhyme-arcade/src/App.jsx
+++ rhyme-arcade/src/App.jsx
@@ -2,14 +2,28 @@ import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import { Auth0Provider } from '@auth0/auth0-react';
 import PlatformSelection from './PlatformSelection';
-import GameSelection from './GameSelection';
+import { fetchGames } from './GameSelection';
 import SpotifyIntegration from './SpotifyIntegration';
 import './App.css'
 
 function App() {
   const [selectedPlatform, setSelectedPlatform] = React.useState(null);
   const [selectedGame, setSelectedGame] = React.useState(null);
-
+  const [games, setGames] = useState([]);
+  
+    useEffect(() => {
+      const fetchData = async () => {
+        try {
+          const data = await fetchGames();
+          setGames(data.results);
+        } catch (error) {
+          // Handle error
+        }
+      };
+  
+      fetchData();
+    }, []);
+  
 
   return (
     <div>
@@ -32,7 +46,15 @@ function App() {
         <SpotifyIntegration selectedGame={selectedGame} />
       )}
       {SpotifyIntegration}
-    
+      
+    <div>
+      <h1>Game List</h1>
+      <ul>
+        {games.map((game) => (
+          <li key={game.id}>{game.name}</li>
+        ))}
+      </ul>
+    </div>
 
 
     </div>
